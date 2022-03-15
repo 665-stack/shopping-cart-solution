e12c56717a26578253fb09c6f78450b1a737ae58
unknown <mahmodulshahin665@gmail.com>
2022-03-15 17:14:08 +0600

    complete phone section

--- app.js
+++ app.js
@@ -1,26 +1,33 @@
-function updateCaseNumber(isIncreasing) {
-    const caseInput = document.getElementById('case-number');
+function updateProductNumber(product, price, isIncreasing) {
+    const productInput = document.getElementById(product + '-number');
     //kono akta var er value jodi reset kora hoy taile shei var ta let diye dite hobe. const diye hobe na. and amra kinto niche if else er modder var er value reset korteci;
-    let caseNumber = caseInput.value;
-    console.log(caseNumber)
+    let productNumber = productInput.value;
+    console.log(productNumber)
     if (isIncreasing == true) {
-        caseNumber = parseInt(caseNumber) + 1;
+        productNumber = parseInt(productNumber) + 1;
     }
-    else if (caseNumber > 0) {
-        caseNumber = parseInt(caseNumber) - 1;
+    else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
     }
-    caseInput.value = caseNumber;
-    //update case total
-    const caseTotal = document.getElementById('case-total');
-    console.log(caseTotal.innerText)
-    caseTotal.innerText = caseNumber * 59;
-
+    productInput.value = productNumber;
+    //update product total
+    const productTotal = document.getElementById(product + '-total');
+    console.log(productTotal.innerText)
+    productTotal.innerText = productNumber * price;
 }
+//phone plus button handler
+document.getElementById('phone-plus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, true)
+})
+document.getElementById('phone-minus').addEventListener('click', function () {
+    updateProductNumber('phone', 1219, false)
+})
+
 //case plus button handler
 document.getElementById('case-plus').addEventListener('click', function () {
-    updateCaseNumber(true);
+    updateProductNumber('case', 59, true);
 })
 //case minus button handler
 document.getElementById('case-minus').addEventListener('click', function () {
-    updateCaseNumber(false)
+    updateProductNumber('case', 59, false)
 })
