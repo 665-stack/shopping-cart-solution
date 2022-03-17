91a7fb6b9fecefe4bcf1237a5465bb980d310d2f
unknown <mahmodulshahin665@gmail.com>
2022-03-17 08:31:28 +0600

    fix some code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function updateProductNumber(product, price, isIncreasing) {
     const productInput = document.getElementById(product + '-number');
     //kono akta var er value jodi reset kora hoy taile shei var ta let diye dite hobe. const diye hobe na. and amra kinto niche if else er modder var er value reset korteci;
     let productNumber = productInput.value;
-    console.log(productNumber)
+    console.log('product number', productNumber)
     if (isIncreasing == true) {
         productNumber = parseInt(productNumber) + 1;
     }
@@ -12,17 +12,19 @@ function updateProductNumber(product, price, isIncreasing) {
     productInput.value = productNumber;
     //update product total
     const productTotal = document.getElementById(product + '-total');
-    console.log(productTotal.innerText)
     productTotal.innerText = productNumber * price;
+    console.log('product total', productTotal.innerText)
+
 }
 //phone plus button handler
 document.getElementById('phone-plus').addEventListener('click', function () {
     updateProductNumber('phone', 1219, true)
 })
+//phone minus button handler
 document.getElementById('phone-minus').addEventListener('click', function () {
     updateProductNumber('phone', 1219, false)
 })
-
+//=============================================
 //case plus button handler
 document.getElementById('case-plus').addEventListener('click', function () {
     updateProductNumber('case', 59, true);
